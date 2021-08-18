4e48bfd04d8550495e8ce70bf73603d510103e3e
Jegan B <87298700+Jamesgosling2004@users.noreply.github.com>
2021-08-18 09:55:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
-
 # Just a basic script U can improvise lateron asper ur need xD 
 
-MANIFEST="git:https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
-DEVICE=phoenix
+abort() { echo "$1"; exit 1; }
+
+MANIFEST="git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0"
+DT_PATH=device/realme/RMX2121
 DT_LINK="https://github.com/Jamesgosling2004/TWRP_RMX2121"
-DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
+mkdir ~/twrp10
+cd ~/twrp10
 apt install openssh-server -y
 apt update --fix-missing
 apt install openssh-server -y
-mkdir ~/shrp && cd ~/shrp
+DEVICE=${DT_PATH##*\/}
 
 echo " ===+++ Syncing Recovery Sources +++==="
-repo init --depth=1 -u $MANIFEST
+repo init --depth=1 -u $MANIFEST -g default,-device,-mips,-darwin,-notdefault 
 repo sync -j$(nproc --all)
 git clone --depth=1 $DT_LINK $DT_PATH
 
@@ -22,8 +24,8 @@ echo " ===+++ Building Recovery +++==="
 export ALLOW_MISSING_DEPENDENCIES=true
 . build/envsetup.sh
 echo " source build/envsetup.sh done"
-lunch omni_${DEVICE}-eng || abort " lunch failed with exit status $?"
-echo " lunch omni_${DEVICE}-eng done"
+lunch twrp_${DEVICE}-eng || abort " lunch failed with exit status $?"
+echo " lunch twrp_${DEVICE}-eng done"
 mka recoveryimage || abort " mka failed with exit status $?"
 echo " mka recoveryimage done"
 
@@ -37,3 +39,7 @@ mv recovery.img ${OUTFILE%.zip}.img
 zip -r9 $OUTFILE ${OUTFILE%.zip}.img
 
 curl -T $OUTFILE https://oshi.at
+
+
+
+
